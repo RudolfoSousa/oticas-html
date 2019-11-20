aae4e038b2e46560076f4d617d6e05df34ae6201
Rudolfo <rudolfosousa@gmail.com>
2019-11-20 14:28:49 -0300

    a empresa - slide 1920 sem icones

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
-var mySwiper = new Swiper ('.swiper-container', {
+$(document).ready(function(){
+  var mySwiper = new Swiper ('.swiper-container.swiper-webdoor', {
     // Optional parameters
     direction: 'vertical',
     loop: true,
 
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-webdoor .swiper-pagination',
     },
 
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-webdoor .swiper-button-next',
+      prevEl: '.swiper-webdoor .swiper-button-prev',
     },
+  });
 
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })
+var swiperBrands = new Swiper ('.swiper-container.swiper-brands', {
+  direction: 'horizontal',
+  loop: false,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-brands .swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-brands .swiper-button-next',
+    prevEl: '.swiper-brands .swiper-button-prev',
+  },
+  slidesPerView: 5,
+  centeredSlides: true,
+  initialSlide: 1
+})
+})
